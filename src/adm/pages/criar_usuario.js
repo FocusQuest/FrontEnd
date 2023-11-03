a5df993c86ee1f88ad84f9f0906d2afbf9fd5ebe
Silmara <silmarabittencourt13@gmail.com>
2023-11-03 17:23:39 -0300

    Modificação da página 'criar usuário'

--- src/adm/pages/criar_usuario.js
+++ src/adm/pages/criar_usuario.js
@@ -20,7 +20,7 @@ const CriarUsuario = () => {
 
   const handleSignup = async () => {
     try {
-      if (!email | !emailConf | !senha) {
+      if (!email || !emailConf || !senha) {
         setError("Preencha todos os campos");
         return;
       } else if (email !== emailConf) {
@@ -49,10 +49,10 @@ const CriarUsuario = () => {
 
       const responseCadastro = await axios.post(
         `http://localhost:3000/usuarios`,
-        data,
+        data
       );
       if (responseCadastro.status === 201) {
-        alert("Usuário cadatrado com sucesso!");
+        alert("Usuário cadastrado com sucesso!");
         navigate("/");
       }
     } catch (error) {}
@@ -60,35 +60,83 @@ const CriarUsuario = () => {
 
   return (
     <C.Container>
-      <C.Label>Crie seu cadastro</C.Label>
-      <C.Content>
-        <Input
-          type="email"
-          placeholder="Digite seu e-mail"
-          value={email}
-          onChange={(e) => [setEmail(e.target.value), setError("")]}
-        />
-        <Input
-          type="email"
-          placeholder="Confirme seu e-mail"
-          value={emailConf}
-          onChange={(e) => [setEmailConf(e.target.value), setError("")]}
-        />
-        <Input
-          type="password"
-          placeholder="Digite sua senha"
-          value={senha}
-          onChange={(e) => [setSenha(e.target.value), setError("")]}
-        />
+      
+      <div className="FormContainerCustom">
+
+      <div>
+        <h2> Abrir chamado</h2>
+      </div>
+
+
+      <div className="Label">
+          <span className="DescriptionCustom">Insira o nome:</span>
+          <input
+            type="text"
+            placeholder="Digite o nome"
+            value={nome}
+            onChange={(e) => [setNome(e.target.value), setError("")]}
+          />
+        </div>
+
+      <div className="Label">
+          <span className="DescriptionCustom">Insira o telefone</span>
+          <input
+            type="text"
+            placeholder="Insira o telefone"
+            value={telefone}
+            onChange={(e) => [setTelefone(e.target.value), setError("")]}
+          />
+        </div>
+
+        <div className="Label">
+          <span className="DescriptionCustom">Insira o email:</span>
+          <input
+            type="email"
+            placeholder="Digite seu e-mail"
+            value={email}
+            onChange={(e) => [setEmail(e.target.value), setError("")]}
+          />
+        </div>
+
+        <div className="Label">
+          <span className="DescriptionCustom">Confirme o email</span>
+          <input
+            type="email"
+            placeholder="Confirme seu e-mail"
+            value={emailConf}
+            onChange={(e) => [setEmailConf(e.target.value), setError("")]}
+          />
+        </div>
+
+        <div className="Label">
+          <span className="DescriptionCustom">Insira a senha</span>
+          <input
+            type="password"
+            placeholder="Digite sua senha"
+            value={senha}
+            onChange={(e) => [setSenha(e.target.value), setError("")]}
+          />
+        </div>
+
+        <div className="Label">
+          <span className="description">Nível de acesso</span>
+          <select
+
+          >
+            <option value="0">Selecione...</option>
+            <option value="1">Usuário</option>
+            <option value="2">Técnico</option>
+            <option value="3">Administrador</option>
+          </select>
+        </div>
+
+      </div>
+
+
         <C.labelError>{error}</C.labelError>
-        <Button Text="Inscrever-se" onClick={handleSignup} />
-        <C.LabelSignin>
-          Já tem uma conta?
-          <C.Strong>
-            <Link to="/">&nbsp;Entre</Link>
-          </C.Strong>
-        </C.LabelSignin>
-      </C.Content>
+        <div className="buttomcontainer">
+        <Button Text="Criar cadastro" onClick={handleSignup} />
+      </div>
     </C.Container>
   );
 };
