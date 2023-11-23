e94272788189007662761f8317c85ce2fca13556
Katiane <katiane.soares@fatec.sp.gov.br>
2023-11-23 15:36:32 -0300

    adicionei a funcionalidade de excluir ususario

--- src/adm/pages/usuariosList.tsx
+++ src/adm/pages/usuariosList.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import '../../css/table.css';
 import { Link } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 interface Usuario {
   id: number;
@@ -12,22 +13,31 @@ interface Usuario {
 
 const UsuariosList: React.FC = () => {
   const [usuarios, setUsuarios] = useState<Usuario[]>([]);
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+  const { id } = useParams();
+  
   const handleButtonClick = (id: number) => {
     // Lógica para lidar com o clique do botão
     console.log(`Botão para alterar o usuário ${id}`);
   };
 
+  
   useEffect(() => {
     axios
-      .get('http://localhost:3000/usuarios')
+      .get(`http://localhost:3000/usuarios`)
       .then((response) => setUsuarios(response.data.usuarios))
       .catch((error) => console.error(error));
   }, []);
-
-  // Add a conditional rendering check
-  if (usuarios.length === 0) {
-    return <div>Loading...</div>;
-  }
+  
+  const handleExcluirUsuario = async (id: number) => {
+    try {
+      await axios.delete(`http://localhost:3000/usuarios/${id}`);
+      setUsuarios(usuarios.filter((usuario) => usuario.id !== id));
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
   
@@ -63,16 +73,18 @@ const UsuariosList: React.FC = () => {
                 </button>
               </Link>
             </td>
-            <td><button 
-                  style={{ 
-                    display: 'block', 
-                    marginLeft: 'auto', 
-                    marginRight: 'auto' 
-                  }}
-                  onClick={() => handleButtonClick(Usuario.id)}
-                >
-                  Excluir
-                </button></td>           
+            <td>
+              <button 
+                style={{ 
+                  display: 'block', 
+                  marginLeft: 'auto', 
+                  marginRight: 'auto' 
+                }}              
+                onClick={() => handleExcluirUsuario(Usuario.id)}
+              >
+                Excluir
+              </button>  
+            </td>              
             </tr>
           ))}
         </tbody>
